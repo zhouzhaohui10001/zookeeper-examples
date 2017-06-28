a0af5daff39f9d2f5071a64ded12794b64c638b1
周井江-技术中心-服务研发部 <zhoujingjiang@gomeplus.com>
2017-06-28 16:39:12 +0800

    fix bug

--- src/main/java/cn/timd/zookeeper/ConfigurationManagement.java
+++ src/main/java/cn/timd/zookeeper/ConfigurationManagement.java
@@ -3,36 +3,55 @@ package cn.timd.zookeeper;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.api.CuratorEvent;
 import org.apache.curator.framework.api.CuratorListener;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.framework.state.ConnectionStateListener;
+import org.apache.zookeeper.Watcher;
 
 public class ConfigurationManagement extends BaseConfiguration {
     private static final String namespace = "service/common-configuration";
     private static final String nodeName = "test";
     private static final String charSet = "utf8";
+    private final Object condition = new Object();
     private volatile String configuration;
 
-    private final CuratorListener listener = new CuratorListener() {
+    private final CuratorListener curatorListener= new CuratorListener() {
         public void eventReceived(CuratorFramework curatorFramework, CuratorEvent curatorEvent) throws Exception {
-            updateConfiguration(curatorFramework, curatorEvent);
+            if (curatorEvent.getWatchedEvent().getType().compareTo(
+                    Watcher.Event.EventType.NodeDataChanged) == 0) {
+                System.out.println(curatorEvent);
+                System.out.println(Thread.currentThread().getName());
+                synchronized (condition) {
+                    updateConfiguration();
+                    condition.notifyAll();
+                }
+            }
+        }
+    };
+
+    private final ConnectionStateListener connectionStateListener = new ConnectionStateListener() {
+        public void stateChanged(CuratorFramework curatorFramework, ConnectionState connectionState) {
+            System.out.println(connectionState);
+            System.out.println(Thread.currentThread().getName());
+            switch (connectionState) {
+                case CONNECTED:
+                case RECONNECTED:
+                    synchronized (condition) {
+                        updateConfiguration();
+                        condition.notifyAll();
+                    }
+                    break;
+            }
         }
     };
 
     {
         client.start();
-        try {
-            configuration = new String(
-                    client.usingNamespace(namespace)
-                            .getData()
-                            .watched()
-                            .forPath(nodeName),
-                    charSet);
-            client.getCuratorListenable().addListener(listener);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            throw new RuntimeException(ex.getMessage());
-        }
+        client.getConnectionStateListenable().addListener(connectionStateListener);
+        client.getCuratorListenable().addListener(curatorListener);
     }
 
-    private void updateConfiguration(CuratorFramework curatorFramework, CuratorEvent curatorEvent) {
+    private void updateConfiguration() {
+        System.out.println("update configuration");
         try {
             configuration = new String(
                     client.usingNamespace(namespace)
@@ -46,20 +65,20 @@ public class ConfigurationManagement extends BaseConfiguration {
         }
     }
 
-    public void work() {
+    public void work() throws Exception {
+        synchronized (condition) {
+            if (configuration == null)
+                condition.wait();
+        }
+
         while (true) {
             System.out.println("Configuration is: " + configuration);
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-                break;
-            }
+            Thread.sleep(2000);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Throwable {
+        System.out.println(Thread.currentThread().getName());
         new ConfigurationManagement().work();
     }
 }
